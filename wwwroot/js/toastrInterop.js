b2734519de6bc1e7173e5373e81422648d4300f7
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-09-12 15:45:09 +0200

    Agregar archivos de proyecto.

---
+++ wwwroot/js/toastrInterop.js
@@ -0,0 +1,17 @@
+﻿// wwwroot/js/toastrInterop.js
+
+window.toastrInterop = function (message, title)
+{
+    if (message == "success") {
+        toastr.success(title);
+    }
+    if (message == "error") {
+        toastr.error(title);
+    }
+    if (message == "warning") {
+        toastr.warning(title);
+    }
+    if (message == "info") {
+        toastr.info(title);
+    }
+}
